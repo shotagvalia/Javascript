d362b5476aff452c88114084ffd97a3d7e1a6fe0
shotagvalia <135604439+shotagvalia@users.noreply.github.com>
2023-06-14 16:33:03 +0400

    Update lesson12.js

--- lesson12.js
+++ lesson12.js
@@ -1,10 +1,13 @@
+const prompt = require(`prompt-sync`)();
+
+let x = prompt(`Pick a number`);
+
 function number(x){
-if(x % 2 == 0){
-    return "Even";
-} else {
-    return "Odd";
-}
+    if(x % 2 == 0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
 }
 
-console.log(number(5));
-console.log(number(6));
+console.log(number(x));
